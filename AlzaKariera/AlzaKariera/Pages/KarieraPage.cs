d8f20c8cb11e32746b9e12e046a3f13fd00b49c8
mezikk <symbian25@gmail.com>
2020-12-09 00:08:28 +0100

    initial commit

---
+++ AlzaKariera/AlzaKariera/Pages/KarieraPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+
+namespace AlzaKariera
+{
+    public class KarieraPage
+    {
+        public KarieraPage(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        public IWebElement DepartmentsList => Driver.FindElement(By.ClassName("departments-list"));
+        
+        public IWebElement OfferList => Driver.FindElement(By.XPath("//job-offer-list//*[@class='container']"));
+
+        public KarieraPage SelectDepartment(string department)
+        {
+            IWebElement input = DepartmentsList.FindElement(By.XPath("//div/input[@value='" + department + "']"));
+            string order = input.GetAttribute("id");
+            IWebElement img = DepartmentsList.FindElement(By.XPath("//*[@for='" + order + "']/img"));
+            img.Click();
+            return this;
+        }
+
+        public KarieraPage SelectSubDepartment(string subDepartment)
+        {
+            IWebElement lnk = OfferList.FindElement(By.XPath("//*[contains(text(), '" + subDepartment + "')]"));
+            lnk.Click();
+            return this;
+        }
+
+    }
+}
---
+++ AlzaKariera/AlzaKariera/Tests/KarieraTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AlzaKariera.Tests
+{
+    class KarieraTest
+    {
+        IWebDriver webDriver = new ChromeDriver();
+
+        [SetUp]
+        public void Setup()
+        {
+            webDriver.Navigate().GoToUrl("https://www.alza.cz/kariera");
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            KarieraPage karieraPage = new KarieraPage(webDriver);
+            karieraPage.SelectDepartment("it")
+                       .SelectSubDepartment("Quality Assurance");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //webDriver.Quit();
+        }
+    }
+}
