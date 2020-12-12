1a76f4c94713b308d12fb7d4208a2d259bb1623c
mezikk <symbian25@gmail.com>
2020-12-12 15:26:51 +0100

    pridani remote podpory

--- AlzaKariera/AlzaKariera/Classes/Driver.cs
+++ AlzaKariera/AlzaKariera/Classes/Driver.cs
@@ -2,6 +2,9 @@
 using AlzaKariera.Tests;
 using NLog;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
 using System.IO;
 using YamlDotNet.Serialization;
@@ -57,8 +60,9 @@ namespace AlzaKariera.Classes
 
         private void InitWebDriver()
         {
-            WebDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
-            WebDriver.Navigate().GoToUrl(Properties.Apps["kariera"].Url);
+            string app = Environment.GetEnvironmentVariable("TestApplication");
+            WebDriver = GetWebDriver();
+            WebDriver.Navigate().GoToUrl(Properties.Apps[app].Url);
             WebDriver.Manage().Window.Maximize();
             WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
@@ -66,6 +70,47 @@ namespace AlzaKariera.Classes
         public void Quit()
         {
             WebDriver.Quit();
+        }
+
+        private IWebDriver GetWebDriver()
+        {
+            string browser = Environment.GetEnvironmentVariable("TestBrowser");
+            Uri uri = new Uri(Properties.Grid.Url);
+            bool isRemote = false;
+            try
+            {
+                isRemote = Boolean.Parse(Environment.GetEnvironmentVariable("TestIsRemote"));
+                Logger.Info("Nastavuji hodnotu isRemote na '" + isRemote + "'");
+            }
+            catch (Exception)
+            {
+                Logger.Warn("Nepodarilo se naparsovat hodnotu, budu nastavovat isRemote=false");
+            }
+
+            if (isRemote)
+            {
+                Logger.Info("Poustim remote driver na url '" + uri + "'");
+                if (browser.Equals("chrome"))
+                    return new RemoteWebDriver(uri, new ChromeOptions());
+                else if(browser.Equals("firefox"))
+                    return new RemoteWebDriver(uri, new FirefoxOptions());
+                else
+                    return new RemoteWebDriver(uri, new ChromeOptions());
+            }
+            else
+            {
+                Logger.Info("Poustim local driver");
+                if (browser.Equals("chrome"))
+                    return new OpenQA.Selenium.Chrome.ChromeDriver();
+                else if (browser.Equals("firefox"))
+                {
+                    FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+                    service.Host = "::1";
+                    return new OpenQA.Selenium.Firefox.FirefoxDriver(service);
+                }
+                else
+                    return new OpenQA.Selenium.Chrome.ChromeDriver();
+            }
         }
     }
 }
--- AlzaKariera/run_tests.cmd
+++ AlzaKariera/run_tests.cmd
@@ -1,10 +1,17 @@
 @echo off
 setlocal
 set PropertiesFile=Config/properties.yaml
+#set ProjectFile=AlzaKariera\AlzaKariera.csproj
 set ProjectFile=%1
+rem set TestApplication=kariera
+set TestApplication=%2
+rem set TestIsRemote=true
+set TestIsRemote=%3
+rem set TestBrowser=chrome
+set TestBrowser=%4
 rem nuget restore AlzaKariera.csproj
 rem msbuild AlzaKariera.csproj -t:Clean,Compile,Build /p:OutputPath=c:\Temp\AlzaKariera
 rem nunit3-console c:\Temp\AlzaKariera\AlzaKariera.dll
 dotnet clean %ProjectFile%
-rem dotnet build %ProjectFile%
+dotnet build %ProjectFile%
 dotnet test %ProjectFile%
